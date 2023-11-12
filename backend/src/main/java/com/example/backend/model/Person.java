803fa2e01d66dfdc270576f333f3de2ae130a01c
Ahmed Medhat <96426130+Ahmed9019@users.noreply.github.com>
2023-11-12 23:27:45 +0200

    deleted getPassword

--- backend/src/main/java/com/example/backend/model/Person.java
+++ backend/src/main/java/com/example/backend/model/Person.java
@@ -64,9 +64,6 @@ public class Person {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
 
     public void setPassword(String password) {
         this.password = password;
