7cb9202a03a92aa7e56b4e7fff20c22257cc57c8
Ahmed Medhat <96426130+Ahmed9019@users.noreply.github.com>
2023-11-13 14:04:29 +0200

    Used lombok library for getters and setters

--- backend/src/main/java/com/example/backend/model/Person.java
+++ backend/src/main/java/com/example/backend/model/Person.java
@@ -1,12 +1,18 @@
 package com.example.backend.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 @Table(name = "Persons_Data")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
     @Column(name = "first_name", nullable = false)
     private String firstName;
@@ -15,14 +21,13 @@ public class Person {
     @Column(name = "e_mail", nullable = false, unique = true)
     private String email;
     @Column(name = "password", nullable = false)
+    @Getter(AccessLevel.NONE)
     private String password;
     @Column(name = "date_of_birth", nullable = false)
     private String DOB;
     @Column(name = "photo_link")
     private String photoLink;
 
-    public Person() {
-    }
     public Person(String firstName, String lastName, String email, String password, String DOB, String photoLink) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -30,71 +35,5 @@ public class Person {
         this.password = password;
         this.DOB = DOB;
         this.photoLink = photoLink;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDOB() {
-        return DOB;
-    }
-
-    public void setDOB(String DOB) {
-        this.DOB = DOB;
-    }
-
-    public String getPhotoLink() {
-        return photoLink;
-    }
-
-    public void setPhotoLink(String photoLink) {
-        this.photoLink = photoLink;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", DOB='" + DOB + '\'' +
-                ", photoLink='" + photoLink + '\'' +
-                '}';
     }
 }
