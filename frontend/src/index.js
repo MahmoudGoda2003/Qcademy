a2928e014995c91790932cbb22679fd04a3e4ee4
Mahmoud goda <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-11-13 23:50:05 +0200

    Revert "[Qcad-27] sign up using gmail"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID} > 
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
