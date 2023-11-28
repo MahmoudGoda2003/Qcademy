c2eff00e44d4fbd311a76ddad6f8343e4fc02727
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-11-28 19:08:30 +0200

    remove lombok from the code coverage check

--- backend/src/main/java/com/example/backend/Person/DTO/SignUpDTO.java
+++ backend/src/main/java/com/example/backend/Person/DTO/SignUpDTO.java
@@ -1,10 +1,12 @@
 package com.example.backend.Person.DTO;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class SignUpDTO {
 
     private String firstName;
