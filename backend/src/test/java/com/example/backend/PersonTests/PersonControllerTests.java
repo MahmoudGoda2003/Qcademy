342d928362f100fc14a88efb1d9d8279ccdfc0fb
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2023-11-29 02:24:20 +0200

    Update PersonControllerTests.java

--- backend/src/test/java/com/example/backend/PersonTests/PersonControllerTests.java
+++ backend/src/test/java/com/example/backend/PersonTests/PersonControllerTests.java
@@ -2,13 +2,21 @@ package com.example.backend.PersonTests;
 
 import com.example.backend.AbstractTest;
 import com.example.backend.Person.DTO.SignUpDTO;
+import com.example.backend.Person.PersonController;
+import com.example.backend.exceptions.exceptions.LoginDataNotValidException;
+import com.example.backend.exceptions.exceptions.WrongDataEnteredException;
+import jakarta.mail.MessagingException;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.HttpClientErrorException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +29,8 @@ public class PersonControllerTests extends AbstractTest {
     public void setup() {
         super.setup();
     }
+    @Autowired
+    private PersonController pc;
 
     @Test
     public void signUpNormal() throws Exception {
@@ -41,5 +51,14 @@ public class PersonControllerTests extends AbstractTest {
         assertEquals("{\"error message\":\"Try to sign up again\"}", mvcResult.getResponse().getContentAsString());
     }
 
-
+    @Test
+    public void test_methods_directly() throws MessagingException {
+        ResponseEntity<String> request = pc.signUp("yahya912ahmed@gmail.com");
+        assertEquals(HttpStatus.OK, request.getStatusCode());
+        SignUpDTO signUpDTO = new SignUpDTO("Yahya", "Azzam", "yahya912ahmed@gmail.com", "test", "1-2-1999");
+        signUpDTO.setCode("testing");
+        assertThrowsExactly(WrongDataEnteredException.class, () -> pc.validateOTP(signUpDTO));
+        assertThrows(HttpClientErrorException.class, () -> pc.googleSignIn(null, "token"));
+        assertThrowsExactly(LoginDataNotValidException.class, () -> pc.logIn(null, "klk", "llo"));
+    }
 }
