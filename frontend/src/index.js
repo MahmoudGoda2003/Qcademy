71c7a75681aa8c37883c0e3ceab3a1592fe15abf
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-11-29 07:17:13 +0200

    home ui finished need just to find demo img for create course

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-import { MantineProvider } from '@mantine/core';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <MantineProvider>
+
         <App />
-      </MantineProvider>
+
     </BrowserRouter>
   </React.StrictMode>
 );
