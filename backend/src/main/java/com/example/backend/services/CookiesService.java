0144e110e5083a37dad62c7b7ebdabe00dac05d8
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-12-09 15:56:37 +0200

    add test to test all the functions

--- backend/src/main/java/com/example/backend/services/CookiesService.java
+++ backend/src/main/java/com/example/backend/services/CookiesService.java
@@ -3,6 +3,8 @@ package com.example.backend.services;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Arrays;
+
 public class CookiesService {
 
     //TODO:cookie.setSecure(true);enable later when using https
@@ -13,22 +15,11 @@ public class CookiesService {
         return cookie;
     }
 
-    public Cookie deleteCookie(String name) {
-        Cookie cookie = new Cookie(name, null);
-        cookie.setMaxAge(0);
-        return cookie;
-    }
-
     public Cookie getCookie(HttpServletRequest request, String name) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null) {
-            return null;
-        }
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals(name)) {
-                return cookie;
-            }
-        }
-        return null;
+        return request.getCookies() == null ? null :
+                Arrays.stream(request.getCookies())
+                        .filter(cookie -> cookie.getName().equals(name))
+                        .findFirst()
+                        .orElse(null);
     }
 }
