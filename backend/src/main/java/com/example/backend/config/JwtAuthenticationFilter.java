ab61e534aa52004ca3df5b7aeeb8cbbd74083337
Hesham Youssef <96203482+Hesham-Youssef@users.noreply.github.com>
2023-12-11 22:58:26 +0200

    Qcad 70 create request filter on the backend side subtask security (#36)
    * mostly done
    
    * Security I guess done
    
    still need to do the tests and handle async
    
    * removed unused parameters
    
    * done merging
    
    * tests i guess done
    
    * changing db name
    
    * added some tests to increase coverage
    
    * added some coverage
    
    * hopefully extra coveerage
    
    * sry
    
    * hnnn
    
    * mmmmmm
    
    * jjjjj
    
    * e

---
+++ backend/src/main/java/com/example/backend/config/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.example.backend.config;
+
+import com.example.backend.person.dto.PersonUserDetails;
+import com.example.backend.person.model.Role;
+import com.example.backend.person.service.PersonService;
+import com.example.backend.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final PersonService personService;
+    @Override
+    public void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final Cookie[] cookies = request.getCookies();
+        if(cookies == null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String jwt = null;
+        for (Cookie cookie : cookies) {
+            if ("qcademy".equals(cookie.getName())) {
+                jwt = cookie.getValue();
+            }
+        }
+        if(jwt == null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        Long userId = jwtService.extractUserId(jwt);
+        Role actualRole = personService.getUserRole(userId);
+        if((actualRole != null)
+                && (SecurityContextHolder.getContext().getAuthentication() == null)
+                && (jwtService.isTokenValid(jwt, actualRole)))
+        {
+            UserDetails personUserDetails = new PersonUserDetails(userId, actualRole);
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    personUserDetails,
+                    null,
+                    personUserDetails.getAuthorities()
+            );
+            authToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ backend/src/test/java/com/example/backend/PersonTests/Security/UnAuthorizedAccess.java
@@ -0,0 +1,62 @@
+package com.example.backend.PersonTests.Security;
+
+import com.example.backend.person.dto.LoginDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UnAuthorizedAccess {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    protected String mapToJson(Object o) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(o);
+    }
+    @Test
+    void testAdminEndPointUnAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/test"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void testStudentEndPointUnAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/test"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void testTeacherEndPointUnAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/teacher/test"))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+
+    @Test
+    void testPersonEndPointUnAuthorized() throws Exception {
+        LoginDTO loginDTO = new LoginDTO("example", "test");
+        String input = mapToJson(loginDTO);
+        mockMvc.perform(MockMvcRequestBuilders.post("/person/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(input))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+}
