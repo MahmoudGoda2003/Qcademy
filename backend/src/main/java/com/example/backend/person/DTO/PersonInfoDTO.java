2c0b568c04e1e93dfc5f829d9a5219f92cafa8b9
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-12-28 15:08:51 +0200

    fixed on bug

--- backend/src/main/java/com/example/backend/person/DTO/PersonInfoDTO.java
+++ backend/src/main/java/com/example/backend/person/DTO/PersonInfoDTO.java
@@ -3,12 +3,9 @@ package com.example.backend.person.dto;
 import com.example.backend.person.model.Person;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
 
@@ -17,24 +14,16 @@ import org.modelmapper.ModelMapper;
 public class PersonInfoDTO {
 
     private static final ModelMapper modelMapper = new ModelMapper();
-
     @NotBlank(message = "First name cannot be blank")
     private String firstName;
-
     @NotBlank(message = "Last name cannot be blank")
     private String lastName;
-
     @NotBlank(message = "Email cannot be blank")
     @Email(message = "Invalid email format")
     private String email;
-
     private String photoLink;
-
     @Size(max = 255, message = "Bio should not exceed 255 characters")
     private String bio;
-
-    @NotBlank(message = "Date of birth cannot be Empty")
-    @Pattern(regexp = "^\\d{1,2}-\\d{1,2}-\\d{4}$", message = "Invalid date of birth format. Use DD-MM-YYYY")
     private String dateOfBirth;
 
     public static PersonInfoDTO convert(Person person) {
